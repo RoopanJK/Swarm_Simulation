a12416fc44080466bbc2aafbf0671a0a73bfe232
RoopanJKR <roopanyt@gmail.com>
2022-12-29 17:36:16 +0530

    Completed tf2pose and goalconst

---
+++ src/swarm_simulation/include/goalconst.h
@@ -0,0 +1,124 @@
+#include <unordered_map>
+#include <vector>
+
+enum Axis
+{
+    cx,
+    cy,
+    xi,
+    yi,
+    ic //Induct Closer
+};
+
+class Goal
+{
+public:
+    Axis axis;
+    int point;
+    Goal()
+    {
+        axis = cx;
+        point = 0;
+    }
+    Goal(Axis _axis, float _point)
+    {
+        axis = _axis;
+        point = _point;
+    }
+};
+
+const double xInduct[1] = {0.9381};
+const double yInduct[2] = {0.3528, 0.6534};
+const double cxPoint[7] = {0.8440, 0.7202, 0.6057, 0.5009, 0.3772, 0.2633, 0.1592};
+const double cyPoint[7] = {0.6506, 0.7327, 0.6188, 0.4980, 0.3824, 0.2877, 0.1605};
+
+std::vector<Goal> one_one_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 3), Goal(cy, 2), Goal(ic, 1)};
+std::vector<Goal> one_two_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 3), Goal(cy, 2), Goal(ic, -1)};
+std::vector<Goal> one_three_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 5), Goal(cy, 2), Goal(ic, -1)};
+std::vector<Goal> one_four_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 3), Goal(cy, 4), Goal(ic, 1)};
+std::vector<Goal> one_five_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 3), Goal(cy, 4), Goal(ic, -1)};
+std::vector<Goal> one_six_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 5), Goal(cy, 4), Goal(ic, -1)};
+std::vector<Goal> one_seven_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 3), Goal(cy, 6), Goal(ic, 1)};
+std::vector<Goal> one_eight_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 3), Goal(cy, 6), Goal(ic, -1)};
+std::vector<Goal> one_nine_waypoint = {Goal(cx, 1), Goal(cy, 5), Goal(cx, 5), Goal(cy, 6), Goal(ic, -1)};
+
+
+std::vector<Goal> two_one_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 3), Goal(cy, 2), Goal(ic, 1)};
+std::vector<Goal> two_two_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 3), Goal(cy, 2), Goal(ic, -1)};
+std::vector<Goal> two_three_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 5), Goal(cy, 2), Goal(ic, -1)};
+std::vector<Goal> two_four_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 3), Goal(cy, 4), Goal(ic, 1)};
+std::vector<Goal> two_five_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 3), Goal(cy, 4), Goal(ic, -1)};
+std::vector<Goal> two_six_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 5), Goal(cy, 4), Goal(ic, -1)};
+std::vector<Goal> two_seven_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 3), Goal(cy, 6), Goal(ic, 1)};
+std::vector<Goal> two_eight_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 3), Goal(cy, 6), Goal(ic, -1)};
+std::vector<Goal> two_nine_waypoint = {Goal(cx, 1), Goal(cy, 3), Goal(cx, 5), Goal(cy, 6), Goal(ic, -1)};
+
+// Return from Goal Points
+
+std::vector<Goal> r_one_one_waypoint = {Goal(cx, 3), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+std::vector<Goal> r_one_two_waypoint = {Goal(cx, 3), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+std::vector<Goal> r_one_three_waypoint = {Goal(cx, 5), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+std::vector<Goal> r_one_four_waypoint = {Goal(cx, 3), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+std::vector<Goal> r_one_five_waypoint = {Goal(cx, 3), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+std::vector<Goal> r_one_six_waypoint = {Goal(cx, 5), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+std::vector<Goal> r_one_seven_waypoint = {Goal(cx, 3), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+std::vector<Goal> r_one_eight_waypoint = {Goal(cx, 3), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+std::vector<Goal> r_one_nine_waypoint = {Goal(cx, 5), Goal(cy, 7), Goal(cx, 1), Goal(yi, 1), Goal(xi, 1)};
+
+
+std::vector<Goal> r_two_one_waypoint = {Goal(cx, 3), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+std::vector<Goal> r_two_two_waypoint = {Goal(cx, 3), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+std::vector<Goal> r_two_three_waypoint = {Goal(cx, 5), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+std::vector<Goal> r_two_four_waypoint = {Goal(cx, 3), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+std::vector<Goal> r_two_five_waypoint = {Goal(cx, 3), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+std::vector<Goal> r_two_six_waypoint = {Goal(cx, 5), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+std::vector<Goal> r_two_seven_waypoint = {Goal(cx, 3), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+std::vector<Goal> r_two_eight_waypoint = {Goal(cx, 3), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+std::vector<Goal> r_two_nine_waypoint = {Goal(cx, 5), Goal(cy, 1), Goal(cx, 1), Goal(yi, 1), Goal(xi, 2)};
+
+
+// Unordered Table (Key Value pairs)
+
+std::unordered_map<std::string, std::vector<Goal>> umap = {
+    {"1_1", one_one_waypoint},
+    {"1_2", one_two_waypoint},
+    {"1_3", one_three_waypoint},
+    {"1_4", one_four_waypoint},
+    {"1_5", one_five_waypoint},
+    {"1_6", one_six_waypoint},
+    {"1_7", one_seven_waypoint},
+    {"1_8", one_eight_waypoint},
+    {"1_9", one_nine_waypoint},
+    {"2_1", two_one_waypoint},
+    {"2_2", two_two_waypoint},
+    {"2_3", two_three_waypoint},
+    {"2_4", two_four_waypoint},
+    {"2_5", two_five_waypoint},
+    {"2_6", two_six_waypoint},
+    {"2_7", two_seven_waypoint},
+    {"2_8", two_eight_waypoint},
+    {"2_9", two_nine_waypoint},
+    {"r_1_1", r_one_one_waypoint},
+    {"r_1_2", r_one_two_waypoint},
+    {"r_1_3", r_one_three_waypoint},
+    {"r_1_4", r_one_four_waypoint},
+    {"r_1_5", r_one_five_waypoint},
+    {"r_1_6", r_one_six_waypoint},
+    {"r_1_7", r_one_seven_waypoint},
+    {"r_1_8", r_one_eight_waypoint},
+    {"r_1_9", r_one_nine_waypoint},
+    {"r_2_1", r_two_one_waypoint},
+    {"r_2_2", r_two_two_waypoint},
+    {"r_2_3", r_two_three_waypoint},
+    {"r_2_4", r_two_four_waypoint},
+    {"r_2_5", r_two_five_waypoint},
+    {"r_2_6", r_two_six_waypoint},
+    {"r_2_7", r_two_seven_waypoint},
+    {"r_2_8", r_two_eight_waypoint},
+    {"r_2_9", r_two_nine_waypoint},
+};
+
+
+
+
+
---
+++ src/swarm_simulation/src/tf2pose.cpp
@@ -0,0 +1,63 @@
+#include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <turtlesim/Pose.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+double roll, pitch, yaw;
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "robot_tf2Pose");
+    ros::NodeHandle n;
+    
+    std::cout<<"Initiating lookup for bot: "<< argv[1];
+    
+    std::string marker_id = "marker_id";
+    std::string bot_id = marker_id.append(argv[1]);
+
+    std::string bot_publihser = "swarmbot";
+    bot_publihser.append(argv[1]);
+    bot_publihser.append("/pose");
+
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tflistener(tfBuffer);
+
+    ros::Publisher swarmbot_pose = n.advertise<turtlesim::Pose>(bot_publihser, 1);
+
+    ros::Rate rate(10.0);
+
+    while(n.ok())
+    {
+        geometry_msgs::TransformStamped transform;
+        try{
+            transform = tfBuffer.lookupTransform("reference", bot_id, ros::Time(0));
+        }
+        catch (tf2::TransformException &ex)
+        {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
+
+        turtlesim::Pose msg;
+        tf2::Quaternion quat;
+        quat = {transform.transform.rotation.x, transform.transform.rotation.y,
+                transform.transform.rotation.z, transform.transform.rotation.w};
+        
+        tf2::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+
+        msg.x = transform.transform.translation.x;
+        msg.y = transform.transform.translation.y;
+        msg.theta = yaw;
+        std::cout << "x= " << msg.x << std::endl;
+        std::cout << "y = "<< msg.y << std::endl;
+        std::cout << "z = "<< yaw << std::endl;
+        
+        swarmbot_pose.publish(msg);
+    }
+}
+
+
